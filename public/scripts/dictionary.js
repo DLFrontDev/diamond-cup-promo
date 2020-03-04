030787ba880310f319a8aa662b7cffed6c02a2ec
Diogo L <diogocardosoluis@gmail.com>
2020-03-04 00:07:59 +0000

    Add hotel info, add svg assets

--- public/scripts/dictionary.js
+++ public/scripts/dictionary.js
@@ -6,10 +6,16 @@ const dict = {
     mainText: {
       date: '2nd & 3rd May, 2020'
     },
+    sideText: {
+      card: 'IFBB Elite Pro Cards awarded to Overall Winners',
+      ranking: 'This championship awards points for the International Amateur Ranking',
+      hotelTitle: 'Lodgings in Lisbon',
+      hotelText: 'There are a few hotels and guesthouses within walking distance of the championship venue. There are also several hotels in the nearby Campo Pequeno area, accessible by metro or a longer walk.'
+    },
     timer: {
-      daysLeft: ' days left until the event',
+      daysLeft: ' days left until the championship',
       daysAnd: ' days and',
-      left: ' left until the event'
+      left: ' left until the championship'
     }
   },
   pt: {
@@ -19,10 +25,16 @@ const dict = {
     mainText: {
       date: '2 & 3 de Maio, 2020'
     },
+    sideText: {
+      card: 'Oferta de IFBB Elite Pro cards aos vencedores de overall',
+      ranking: 'Este campeonato atribui pontos ao Ranking Amador Internacional',
+      hotelTitle: 'Alojamento em Lisboa',
+      hotelText: 'Há alguns hoteis e casas de hóspedes a curta distância do local do campeonato. Também há vários hóteis na área de Campo Pequeno, acessível por metro ou uma caminhada mais longa.'
+    },
     timer: {
-      daysLeft: ' dias até ao evento começar',
+      daysLeft: ' dias até o campeonato começar',
       daysAnd: ' dias e',
-      left: ' até ao evento começar'
+      left: ' até o campeonato começar'
     }
   }
 };
